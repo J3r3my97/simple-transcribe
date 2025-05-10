7d1026f538789e6266a7cb2509ce3b0e7c243355
iamjerry1027 <jerry.assan@getbrewbox.com>
2025-05-09 21:53:23 -0400

    remove duplicate

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { User } from '@supabase/supabase-js'
-import { createSupabaseClient } from '../lib/supabase'
+import { createSupabaseClient } from '../../lib/supabase'
 
 interface AuthContextType {
     user: User | null
